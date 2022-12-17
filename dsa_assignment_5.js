3e4ea35988e1223846848dae4a0a530d9a96bcc5
James Kyan <james.kyan88@gmail.com>
2022-12-17 10:18:43 -0500

    completed kind of

--- dsa_assignment_5.js
+++ dsa_assignment_5.js
@@ -4,44 +4,44 @@
 // 1.Merge sort (ascending order) by the rank
 // 2.Merge sort (ascending order) by the ratings AND rank
 // Please see below for the data returned from the database
-// const data = {
-//     results: {
-//         payload: [
-//             {
-//                 profile: {
-//                     name: 'Tony',
-//                     rank: 9,
-//                     favorites: [
-//                         {
-//                             title: 'Spider-Man',
-//                             rating: 5
-//                         },
-//                         {
-//                             title: 'Spongebob',
-//                             rating: 9
-//                         }
-//                     ]
-//                 }
-//             },
-//             {
-//                 profile: {
-//                     name: 'John',
-//                     rank: 2,
-//                     favorites: [
-//                         {
-//                             title: 'Hulk',
-//                             rating: 1
-//                         },
-//                         {
-//                             title: 'Top Gun',
-//                             rating: 8
-//                         }
-//                     ]
-//                 }
-//             }
-//         ]
-//     }
-// }
+const data = {
+    results: {
+        payload: [
+            {
+                profile: {
+                    name: 'Tony',
+                    rank: 9,
+                    favorites: [
+                        {
+                            title: 'Spider-Man',
+                            rating: 5
+                        },
+                        {
+                            title: 'Spongebob',
+                            rating: 9
+                        }
+                    ]
+                }
+            },
+            {
+                profile: {
+                    name: 'John',
+                    rank: 2,
+                    favorites: [
+                        {
+                            title: 'Hulk',
+                            rating: 1
+                        },
+                        {
+                            title: 'Top Gun',
+                            rating: 8
+                        }
+                    ]
+                }
+            }
+        ]
+    }
+}
 // console.log(data.results.payload[1].profile.rank)
 // data.results.payload[1].profile.rank
 
@@ -50,18 +50,67 @@
 // compare the ranking of each array
 // merge the array back 
 
-// const sortThisByRank = data.results.payload.sort(
-//     (rankA, rankB) => rankA.profile.rank - rankB.profile.rank,
-// )
-// console.log(sortThisByRank)
+function merge(arr1, arr2) {
+    // Create a new empty array to store the merged result
+    let merged = [];
 
-// const mergesort = (sortThisByRank) => {
+    // Set variables to track the current index in each array
+    let i = 0;
+    let j = 0;
 
-//     let left = sortThisByRank[0]
-//     let right = sortThisByRank[1]
+    // Loop while there are still elements in either array
+    while (i < arr1.length || j < arr2.length) {
+        // If both arrays have elements, compare the elements and add the smaller one to the merged array
+        if (i < arr1.length && j < arr2.length) {
+            if (arr1[i].profile.favorites[0].rating < arr2[j].profile.favorites[0].rating ||
+                (arr1[i].profile.favorites[0].rating === arr2[j].profile.favorites[0].rating && arr1[i].profile.rank < arr2[j].profile.rank)) {
+                merged.push(arr1[i]);
+                i++;
+            } else {
+                merged.push(arr2[j]);
+                j++;
+            }
 
-// }
-// console.log(mergesort(sortThisByRank))
+            // If only the first array has elements, add its element to the merged array
+        } else if (i < arr1.length) {
+            merged.push(arr1[i]);
+            i++;
+
+            // If only the second array has elements, add its element to the merged array
+        } else {
+            merged.push(arr2[j]);
+            j++;
+        }
+    }
+
+    // Return the merged array
+    return merged;
+}
+
+function mergeSort(arr) {
+    // If the array has only one element, return it
+    if (arr.length === 1) {
+        return arr;
+    }
+
+    // Split the array into two halves
+    let mid = Math.floor(arr.length / 2);
+    let left = arr.slice(0, mid);
+    let right = arr.slice(mid);
+
+    // Recursively sort the left and right halves
+    left = mergeSort(left);
+    right = mergeSort(right);
+
+    // Merge the sorted left and right halves and return the result
+    return merge(left, right);
+}
+
+// Use the mergeSort function to sort the array of objects by rating and then by rank
+// let sortedData = mergeSort(data.results.payload);
+let sortedData = mergeSort(data.profile.rank);
+
+console.log(sortedData)
 
 // Problem #2: Singly Linked List: 
 // Please construct the two classes with the appropriate properties for the Node and SLL, then for the SLL, 
@@ -69,57 +118,90 @@
 // Push()
 // Pop()
 
-class Node {
-    constructor(val) {
-        this.val = val;
-        this.next = null;
-    }
-}
+// class Node {
+//     constructor(val) {
+//         this.val = val;
+//         this.next = null;
+//     }
+// }
 
-class SinglyLinkedList {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-        this.length = null;
-    }
-    push(val) {
-        let newNode = newNode(val)
+// class SinglyLinkedList {
+//     constructor() {
+//         this.head = null;
+//         this.tail = null;
+//         this.length = null;
+//     }
+//     push(val) {
+//         let newNode = newNode(val)
 
-        if (!this.head) {
-            this.head = newNode
-            this.tail = newNode
-        } else {
-            this.tail.next = newNode
-            this.tail = newNode
-        }
+//         if (!this.head) {
+//             this.head = newNode
+//             this.tail = newNode
+//         } else {
+//             this.tail.next = newNode
+//             this.tail = newNode
+//         }
 
-        this.length++;
-        return this
-    }
-    pop() {
-        if (!this.head) return;
+//         this.length++;
+//         return this
+//     }
+//     pop() {
+//         if (!this.head) return;
 
-        let current = this.head
-        let newTail = current
+//         let current = this.head
+//         let newTail = current
 
-        while (current.next) {
-            newTail = current
-            current = current.next
-        }
+//         while (current.next) {
+//             newTail = current
+//             current = current.next
+//         }
 
-        this.tail = newTail
+//         this.tail = newTail
 
-        this.tail.next = null
+//         this.tail.next = null
 
-        this.length--
+//         this.length--
 
-        if (this.length === 0) {
-            this.head = null
-            this.tail = null
-        }
+//         if (this.length === 0) {
+//             this.head = null
+//             this.tail = null
+//         }
 
-        return current
-    }
-}
+//         return current
+//     }
+// }
 
 
+// Problem #3: Please solve this pattern using time complexity of O(n) and using the CORRECT PATTERN!
+
+//    /**
+//     * @param {number[]} height
+//     * @return {number}
+//     */
+// pointers??
+// height defined
+// height = [1, 8, 6, 2, 5, 4, 8, 3, 7] // 49
+// // height = [1, 1] // 1
+// const maxArea = (height) => {
+//     // two points left and right
+//     let left = 0
+//     let right = height.length - 1
+//     // set max area to 0
+//     let maxWater = 0
+//     // while condition for left < right
+//     while (left < right) {
+//         // determine the lesser of two points
+//         let minHeight = Math.min(height[left], height[right])
+//         // determine max area between max and min height of the two points
+//         maxWater = Math.max(maxWater, minHeight * (right - left))
+//         // conditional to keep points going 
+//         if (height[left] < height[right]) {
+//             left++
+//         } else {
+//             right--
+//         }
+//     }
+//     return maxWater
+// };
+
+// console.log(maxArea(height)) 
